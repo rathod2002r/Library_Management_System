86c6e337d34887cb4364030cf82396e13b28200a
Rathod Rahul <156564332+rathod2002r@users.noreply.github.com>
2025-07-01 11:08:57 +0530

    Libarary.java

---
+++ Libarary.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Library {
+    private ArrayList<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void showAllBooks() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public void issueBook(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title) && !book.isIssued()) {
+                book.issueBook();
+                System.out.println("Book issued: " + title);
+                return;
+            }
+        }
+        System.out.println("Book not available.");
+    }
+
+    public void returnBook(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title) && book.isIssued()) {
+                book.returnBook();
+                System.out.println("Book returned: " + title);
+                return;
+            }
+        }
+        System.out.println("This book wasn't issued.");
+    }
+}
